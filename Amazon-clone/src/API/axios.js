e1ab577c37c3704addd63de11e1988d1e912ae66
HelenMenasie <helenmenassie94@gmail.com>
2024-10-25 19:42:54 +0300

    deploying on netlify

--- Amazon-clone/src/API/axios.js
+++ Amazon-clone/src/API/axios.js
@@ -1,7 +1,16 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://amazon-backend-updated.onrender.com",
+
+  // baseURL: "https://amazon-backend-updated.onrender.com",
+
+  // backend deployed on render
+  baseURL: "https://amazon-api-deploy-dx0e.onrender.com",
 });
 
 export { axiosInstance };
+
+
+ 
+ 
+ 
